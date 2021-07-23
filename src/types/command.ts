4c68d7bdf7a2896672506e645b7e779ed1d9ef56
Michelle S <progamer.firestorm@gmail.com>
2021-07-23 13:41:14 -0600

    Add documentation

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,11 +1,37 @@
+/**
+ * Type representing a command's `execute()` method.
+ */
 type CommandHandler = (args: string[], options: string[]) => void;
+/**
+ * Type representing a command's `execute()` method.
+ */
 type CommandHandlerAsync = (args: string[], options: string[]) => Promise<void>;
 
-// TODO: Add JSDoc comments
+/**
+ * Defines the interface for a command.
+ */
 export default interface Command {
+  /**
+   * The name which is used to call the command.
+   */
   name: string;
+  /**
+   * A list of alternate names which may be used to call the command.
+   */
   aliases: string[];
+  /**
+   * A description of the command's functionality.
+   */
   description: string;
+  /**
+   * The protection level of the command.
+   * - `'normal'` - any user can use the command, and it will be listed by the help command.
+   * - `'secret'` - any user can use the command, but it will not be listed by the help command.
+   * - `'protected'` - only the bot author can use the command, and it will not be listed.
+   */
   protection: 'normal' | 'secret' | 'protected';
+  /**
+   * The function called when the command is executed.
+   */
   execute: CommandHandler | CommandHandlerAsync;
 }
