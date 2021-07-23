67828ffe631b50d710fe3b7a17238e22b5f0bb7c
Michelle S <progamer.firestorm@gmail.com>
2021-07-22 19:17:56 -0600

    Initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { Client, Message } from 'discord.js';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: __dirname + '/.env' });
 
 const client = new Client();
 
 client.once('ready', () => {
   console.log('Ready!');
+});
+
+client.on('message', (message) => {
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
 });
 
 client.login(process.env.TOKEN);
