96c7e3292902b6a04c3ca992259144cffc2bb557
Michelle S <progamer.firestorm@gmail.com>
2021-07-22 19:32:41 -0600

    Adjust imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { Client, Message } from 'discord.js';
+import * as Discord from 'discord.js';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
 dotenv.config({ path: __dirname + '/.env' });
 
-const client = new Client();
+const client = new Discord.Client();
 
 client.once('ready', () => {
   console.log('Ready!');
