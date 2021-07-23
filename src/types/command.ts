de568fa4857ac8f11a78d6d55c02b27f2e96ec6c
Michelle S <progamer.firestorm@gmail.com>
2021-07-23 08:23:09 -0600

    Add function types
    Also rename Command type

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,6 +1,9 @@
-export default interface command {
+type CommandHandler = (args: string[], options: string[]) => void;
+type CommandHandlerAsync = (args: string[], options: string[]) => Promise<void>;
+
+export default interface Command {
   name: string;
   aliases: string[];
   type: 'normal' | 'secret' | 'protected';
-  execute: any; // TODO: Add function type signature.
+  execute: CommandHandler | CommandHandlerAsync;
 }
