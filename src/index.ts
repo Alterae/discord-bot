0959dae21a1c69e3f23523001813b2bef13a34c6
Michelle S <progamer.firestorm@gmail.com>
2021-07-22 18:05:35 -0600

    Set up to use dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import { Client, Message } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client();
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.login(process.env.TOKEN);
